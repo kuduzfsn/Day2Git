29653798202bae42dda89d2f3540c62b8053121c
elkha <elkha@10.0.0.79>
2023-04-07 17:31:33 -0700

    Implemented

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,11 +2,14 @@ package day2;
 
 public class Day2Demo {
 
-	public static void main(String[] args) {
 	
+	
+	public void getInfo() {
 		
-		System.out.println("Day2Demo");
-
+		System.out.println("Get Info");
+		
+		
+		
 	}
 
 }
